e50af1dd8355db61a3ef26cc88a007837cd58cff
Victorlapa3 <victorlapa0p@email.com>
2025-05-26 10:55:04 -0300

    style: add pending orders list

--- src/pages/home/_components/PendingOrderList/index.tsx
+++ src/pages/home/_components/PendingOrderList/index.tsx
@@ -1,13 +1,50 @@
+import {
+    Container,
+    OrderItem,
+} from './styles'
+
+import { Text } from '@/components/@ui/Text'
+
 export interface PendingOrderListProps {
-    orders: { id: string, name: string, date: string}[]
+    orders: { id: string; name: string; date: string }[]
 }
 
 export function PendingOrderList({ orders }: PendingOrderListProps) {
     return (
-        <div>
-            {orders.map(order => (
-                <div>{order.name}, {order.date}</div>
+        <Container>
+            <Text size="md" css={{ color: '$base300' }}>
+                Pedidos pendentes
+            </Text>
+
+            {orders.map((order) => (
+                <OrderItem key={order.id}>
+                    <div>
+                        <Text
+                            size="sm"
+                            css={{
+                                maxWidth: '170px',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                whiteSpace: 'nowrap',
+                                color: '$base100',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {order.name}
+                        </Text>
+
+                        <Text
+                            size="sm"
+                            css={{
+                                fontWeight: '$medium',
+                                color: '$mutedForeground',
+                            }}
+                        >
+                            {order.date}
+                        </Text>
+                    </div>
+                </OrderItem>
             ))}
-        </div>
+        </Container>
     )
 }
