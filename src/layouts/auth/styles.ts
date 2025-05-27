6b6943f0461ff4276369f9e56a1696f26166af3c
matheusbarcc <matheusbarcon@gmail.com>
2025-05-27 16:55:39 -0300

    style: add auth layout visual structure

---
+++ src/layouts/auth/styles.ts
@@ -0,0 +1,42 @@
+import { Card } from "@/components/@ui/Card";
+import { styled } from "@edna-ui/react";
+import Image from "next/image";
+
+export const Container = styled('div', {
+  position: 'relative',
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  padding: '$5'
+})
+
+export const BackgroundImageContainer = styled(Image, {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  zIndex: 0
+})
+
+export const FormContainer = styled(Card, {
+  position: 'relative',
+  zIndex: 1,
+
+  width: '50%',
+  minWidth: '270px',
+  height: '100%',
+
+  padding: 0,
+
+  '@md': {
+    width: '70%'
+  },
+
+  '@sm': {
+    width: '100%'
+  },
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,15 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 import { AppLayout } from '../layouts/App'
 import { Toaster } from 'sonner'
+import { AuthLayout } from '@/layouts/Auth'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
+    <AuthLayout>
       <Toaster richColors position="top-right" />
       <Component {...pageProps} />
-    </AppLayout>
+    </AuthLayout>
   )
 }
