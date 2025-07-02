0669a841daf7433ca5fc9bd25715cdb5e2d90d54
matheusbarcc <matheusbarcon@gmail.com>
2025-07-01 21:49:18 -0300

    feat: add image preview item cover image indicator

--- src/pages/clothes/_components/ClotheForm/ImagePreviewItem/index.tsx
+++ src/pages/clothes/_components/ClotheForm/ImagePreviewItem/index.tsx
@@ -1,16 +1,19 @@
-import { X } from '@phosphor-icons/react'
-import { ClotheImage, Container, RemoveButton } from './styles'
+import { X } from "@phosphor-icons/react";
+import { ClotheImage, Container, RemoveButton } from "./styles";
+import { Text } from "@/components/@ui/Text";
 
 export interface ImagePreviewItemProps {
-  name: string
-  src: string
-  onRemove: (name: string) => void
+  name: string;
+  src: string;
+  onRemove: (name: string) => void;
+  isCover?: boolean;
 }
 
 export function ImagePreviewItem({
   src,
   name,
   onRemove,
+  isCover = false,
 }: ImagePreviewItemProps) {
   return (
     <Container>
@@ -18,6 +21,20 @@ export function ImagePreviewItem({
         <X size={15} weight="bold" />
       </RemoveButton>
       <ClotheImage src={src} width={80} height={80} alt="" unoptimized />
+      {isCover && (
+        <div
+          style={{
+            padding: "2px",
+            marginTop: "-10px",
+            background: "var(--colors-base100)",
+            borderRadius: "0 0 6px 6px",
+          }}
+        >
+          <Text size="xs" style={{ color: "#FFF" }}>
+            Foto Capa
+          </Text>
+        </div>
+      )}
     </Container>
-  )
+  );
 }
